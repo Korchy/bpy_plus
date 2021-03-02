266ea85b01bfdee4223393e74c040ef0cf4e901b
Nikita Akimov <korchiy@yandex.ru>
2021-03-02 15:09:17 +0300

    update

--- objects.py
+++ objects.py
@@ -21,3 +21,10 @@ def deselect_all(context=bpy.context):
     """
     for obj in context.blend_data.objects:
         obj.select_set(False)
+
+def set_active(obj, context=bpy.context):
+    """ Set object as active
+
+    """
+    if obj:
+        context.view_layer.objects.active = obj
