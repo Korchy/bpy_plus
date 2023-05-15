98f2c26f89dc94d5fe1306e598c321444744e78a
Nikita Akimov <korchiy@yandex.ru>
2023-05-15 13:44:27 +0300

    update

--- color.py
+++ color.py
@@ -48,3 +48,46 @@ class Color:
             random.uniform(0, 1),
             random.uniform(0, 1) if alpha else 1.0
         )
+
+    @staticmethod
+    def linear_to_srgb(color_value: float) -> int:
+        """
+        Convert color value frm Linear color space (Blender native) to sRGB color space (commonly used)
+        :param color_value: color value in Linear color space in 0.0 ... 1.0 range
+        :return: color value in sRGB color space in 0 - 255 range
+        """
+        if color_value <= 0.0031308:
+            return int(12.92 * color_value * 255.99)
+        else:
+            return int((1.055 * color_value ** (1 / 2.4) - 0.055) * 255.99)
+
+    @staticmethod
+    def srgb_to_linear(color_value: int) -> float:
+        """
+        Convert color value frm sRGB color space (commonly used) to Linear color space (Blender native)
+        :param color_value: color value in sRGB color space in 0 - 255 range
+        :return: color value in Linear color space in 0.0 - 1.0 range
+        """
+        color_value /= 255.99
+        if color_value <= 0.04045:
+            return color_value / 12.92
+        else:
+            return ((color_value + 0.055) / 1.055) ** 2.4
+
+    @staticmethod
+    def rgba_to_rgb565(rgba: [list, tuple]) -> int:
+        """
+        Convert color from RGBA format to RGB565 format
+        :param rgba: color in RGBA format, list/tuple of 4 color components (R, G, B, A)
+            in range (0-255, 0-255, 0-255, 0-255)
+        :return: color in RGB565 format, 2-bytes int, in 0-65535 range
+        """
+        r, g, b, a = rgba
+        r = r >> 3
+        g = g >> 2
+        b = b >> 3
+        rgb565 = 0x0000
+        rgb565 |= ((r & 0x1F) << 11)
+        rgb565 |= ((g & 0x3F) << 5)
+        rgb565 |= (b & 0x1F)
+        return rgb565
